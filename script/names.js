0dbf68a7ad0056dd79deac1049c4944891d81590
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-31 16:28:00 +0100

    first export

--- script/names.js
+++ script/names.js
@@ -2,5 +2,13 @@
 // 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 // 2. Esporta la funzione dal file.
 
-
-
+// 1 - names.js
+function createFullName(firstName, lastName) {
+    return {
+      firstName: firstName,
+      lastName: lastName
+    };
+  }
+  
+  // 2 - Esportazione della funzione
+  module.exports = {createFullName};
--- script/people.js
+++ script/people.js
@@ -2,3 +2,15 @@
 // 1. Importa la tua funzione da names.js
 // 2. Importa la tua funzione da hobbies.js
 // 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
+
+// 3 - people.js
+const createFullName = require('./names.js');  // Importa la funzione da names.js
+
+function getPerson() {
+    // Usa la funzione per creare il nome completo
+  const fullName = createFullName('alessandro', 'bianchi');  
+  // Restituisci un oggetto con la proprietà fullName
+  return { fullName };  
+}
+
+console.log(getPerson()); 
