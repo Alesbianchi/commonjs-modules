fc8960aa19ba154544dcec8791e66fb50d2b371b
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-31 17:36:17 +0100

    add hobbies export function

--- script/hobbies.js
+++ script/hobbies.js
@@ -1,3 +1,11 @@
 // 2 - hobbies.js
 // 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 // 2. Esporta la funzione dal file.
+
+// 2 - hobbies.js
+function createHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+  
+  // 2 - Esportazione della funzione
+  module.exports = createHobbies;
